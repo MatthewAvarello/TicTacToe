98462ad35a86184576222af1240528a539230dd9
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2025-06-02 14:33:07 -0400

    Implement check win logic.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const gameboard = (function(){
+onst gameboard = (function(){
   let board = [
-  ["0","0","0",],
-  ["","","",],
-  ["","","",]
+  ["7","7","3",],
+  ["7","3","7",],
+  ["3","7","7",]
   ]
   let Reset = function Reset(){
     board = [
@@ -30,18 +30,55 @@ function CreateUser(name,number,marker){
 const gamecontroller = (function(){
   let checkwin = function checkwin(){
     let boardcopy = gameboard.GetBoardStateCopy()
-    if (boardcopy[0].every((value) => value == boardcopy[0][0]) == true && boardcopy[0].every((value) => value != "")){
-      console.log("First row is all the same")
-    } else if ((boardcopy[1].every((value) => value == boardcopy[1][0]) == true && boardcopy[1].every((value) => value != ""))){
-      console.log("Secondrow")
-    } else if((boardcopy[2].every((value) => value == boardcopy[2][0]) == true && boardcopy[2].every((value) => value != ""))){
-      console.log("Thirdrow")
-    } else{
-      console.log("")
+    // check if any row wins and the token
+    for (let i = 0; 2 >= i; i++){
+        if (boardcopy[i].every((value) => value == boardcopy[i][0]) == true && boardcopy[i].every((value) => value != "")){
+            console.log("A Row Won")
+            console.log(boardcopy[i][0])
+            return {Win:true, Token:boardcopy[i][0]}
+        } 
     }
-  }
+    // check if any colums wins and the token
+    for (let i = 0; 2 >= i; i++){
+        let history = []
+        for(let j = 0; 2>= j; j++){
+            history.push(boardcopy[j][i])
+        }
+        if (history.every((value) => value == history[0]) == true && history.every((value) => value != "")){
+            console.log("All values the same in history array")
+            return{Win:true, Token:history[0]}
+        }
+    }
+    // check if any top left to bottom right diagonal wins
+    // Putting code in curly braces so history stays in that scope
+    {
+        let history = []
+        for(let i = 0; 2 >= i; i++){
+            history.push(boardcopy[i][i])
+        }
+        console.log(history)
+        if (history.every((value) => value == history[0]) == true && history.every((value) => value != "")){
+            console.log("diaggggg")
+            return{Win:true, Token:history[0]}
+        }
+    }
+    // Check If any top right to bottem left diagonal wins
+    {
+        // 2 0   1 1  0 2
+        let history = []
+        for (let i = 0; 2 >= i; i++){
+            history.push(boardcopy[i][2 - i])
+        }
+        console.log(history)
+        if (history.every((value) => value == history[0]) == true && history.every((value) => value != "")){
+            console.log("Other Diag")
+            return{Win:true, Token:history[0]}
+        }
+    }
+    }
 
-  return{checkwin}
+    return{checkwin}
+
 })();
 
 console.log(gamecontroller.checkwin())

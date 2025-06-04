02d10d7f707420a16de166540b05b3a18208557e
Matthew Avarello <182387652+MatthewAvarello@users.noreply.github.com>
2025-06-03 23:07:44 -0400

    Added all needed game functions. Need to connect them all and then work on display fucntions.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const gameboard = (function(){
   let board = [
-  ["","","",],
-  ["","","",],
-  ["","","",]
+  ["x","0","x",],
+  ["x","0","0",],
+  ["0","x","x",]
   ]
   let Reset = function Reset(){
     board = [
@@ -13,6 +13,7 @@ const gameboard = (function(){
   }
   let Mark = function Mark(marker,positionx,positiony){
     board[positiony].splice(positionx,1,marker)
+    gamecontroller.switchturn()
   }
   let Log = function Log(){
     console.log(board)
@@ -27,10 +28,11 @@ const DisplayController = (function(){
     let player2input = document.querySelector("#playertwoname")
     let submitbutton = document.querySelector("#PlayerNameButton")
     let Formsubmit = function Formsubmit(event){
+      event.preventDefault()
       let player1inputvalue = player1input.value
       let player2inputvalue = player2input.value
-      let player1 = CreateUser(player1inputvalue,1,"X")
-      let player2 = CreateUser(player2inputvalue,2,"Y")
+      let player1 = CreateUser(player1inputvalue,1,undefined,false)
+      let player2 = CreateUser(player2inputvalue,2,undefined,false)
       gamecontroller.setplayers(player1,player2)
     }
     submitbutton.addEventListener("click",Formsubmit)
@@ -39,8 +41,8 @@ const DisplayController = (function(){
     return{Formsubmit}
 
 })();
-function CreateUser(name,playernumber,marker){
-  return{name,playernumber,marker}
+function CreateUser(name,playernumber,marker,isitcurrentturn){
+  return{name,playernumber,marker,isitcurrentturn}
 }
 
 const gamecontroller = (function(){
@@ -81,6 +83,7 @@ const gamecontroller = (function(){
         }
     }
     // Check If any top right to bottem left diagonal wins
+    // Also check ties here in a else if statement due to all win possibilities being checked
     {
         // 2 0   1 1  0 2
         let history = []
@@ -91,16 +94,59 @@ const gamecontroller = (function(){
         if (history.every((value) => value == history[0]) == true && history.every((value) => value != "")){
             console.log("Other Diag")
             return{Win:true, Token:history[0]}
+        } else if(!boardcopy.includes("")){
+            console.log("Tie")
         }
     }
+    console.log("No win")
+    return{Win:false, Token:undefined}
+    
     }
   let setplayers = function setplayers(p1,p2){
     console.log("TEST")
     player1 = p1
     player2 = p2
-    console.log(player1 + player2)
+    initializegame()
   }
-  return{checkwin,setplayers}
+  let initializegame = function initializegame(){
+    gameboard.Reset()
+    let rng = Math.random()
+    player1.isitcurrentturn = false
+    player2.isitcurrentturn = false
+    if (rng > 0.5){
+      console.log("Player 1 Starts")
+      player1.isitcurrentturn = true
+      player1.marker = "X"
+      player2.marker = "O"
+      console.log(player1)
+    } else{
+      console.log("Player 2 Starts")
+      player2.marker = "X"
+      player1.marker = "O"
+      player2.isitcurrentturn = true
+      console.log(player2)
+    }
+  }
+  let switchturn = function switchturn(){
+    if(player1.isitcurrentturn == true){
+      player1.isitcurrentturn = false
+      player2.isitcurrentturn = true
+    } else if (player2.isitcurrentturn == true){
+      player2.isitcurrentturn = false
+      player1.isitcurrentturn = true
+    } else{
+      console.warn("Error Switching Turn");
+    }
+    console.log(player1,player2)
+  }
+  let playround = function playround(x,y){
+    if (player1.isitcurrentturn == true){
+      board.Mark(player1.marker,x,y)
+    } else if (player2.isitcurrentturn == true){
+      board.Mark(player2.marker,x,y)
+    }
+  }
+  return{checkwin,setplayers,initializegame,switchturn,playround}
 
 })();
 
